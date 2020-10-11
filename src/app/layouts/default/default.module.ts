d6d5233d5bf623f8da11bb322976c8ec7e46e462
Marudhupandian Maruthan <mmp1985@gmail.com>
2020-10-11 19:14:39 +0530

    Initial Commit

---
+++ src/app/layouts/default/default.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DefaultComponent } from './default.component';
+import { DashboardComponent } from './../../modules/dashboard/dashboard.component';
+import { RouterModule } from '@angular/router';
+import { PostsComponent } from './../../modules/posts/posts.component';
+import { SharedModule } from './../../shared/shared.module';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatDividerModule} from '@angular/material/divider';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatCardModule} from '@angular/material/card';
+import {DashboardService} from './../../modules/dashboard.service';
+
+@NgModule({
+  declarations: [
+    DefaultComponent,
+    DashboardComponent,
+    PostsComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    SharedModule,
+    MatSidenavModule,
+    MatDividerModule,
+    FlexLayoutModule,
+    MatCardModule
+  ],
+  providers: [
+    DashboardService
+  ]
+})
+export class DefaultModule { }
